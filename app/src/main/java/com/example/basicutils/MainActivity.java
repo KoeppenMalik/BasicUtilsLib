b5362b99ba2965c72b59a5e3e992aafc44e8e793
KoeppenMalik <Malik.koeppen@web.de>
2021-10-23 23:44:02 +0200

    added permissions request to read and write storage

--- app/src/main/java/com/example/basicutils/MainActivity.java
+++ app/src/main/java/com/example/basicutils/MainActivity.java
@@ -7,9 +7,15 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final int PERMISSION_REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST_CODE = 1;
+
+    public static final String[] REQUIRED_PERMISSIONS = new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean hasFilePermissions() {
-        return ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        return (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                && (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
     }
 
     private void requestFilePermissions() {
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
     }
 }
